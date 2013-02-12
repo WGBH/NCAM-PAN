8687e9808306a19664e4a707e3622f41d9b94980
Mitch Smith <mitch_smith@wgbh.org>
2013-02-12 15:08:36 -0500

    more schema and template changes

--- blacklight-app/app/controllers/catalog_controller.rb
+++ blacklight-app/app/controllers/catalog_controller.rb
@@ -1,7 +1,7 @@
 # -*- encoding : utf-8 -*-
 require 'blacklight/catalog'
 
-class CatalogController < ApplicationController  
+class CatalogController < ApplicationController
 
   include Blacklight::Catalog
 
@@ -14,18 +14,17 @@ class CatalogController < ApplicationController
 
     ## Default parameters to send on single-document requests to Solr. These settings are the Blackligt defaults (see SolrHelper#solr_doc_params) or 
     ## parameters included in the Blacklight-jetty document requestHandler.
-    #
-    #config.default_document_solr_params = {
-    #  :qt => 'document',
-    #  ## These are hard-coded in the blacklight 'document' requestHandler
-    #  # :fl => '*',
-    #  # :rows => 1
-    #  # :q => '{!raw f=id v=$id}' 
-    #}
+    config.default_document_solr_params = {
+      :qt => 'document',
+      ## These are hard-coded in the blacklight 'document' requestHandler
+      # :fl => '*',
+      # :rows => 1
+      # :q => '{!raw f=id v=$id}' 
+    }
 
     # solr field configuration for search results/index views
     config.index.show_link = 'title_s'
-    #config.index.record_display_type = 'format'
+    config.index.record_display_type = 'description_s'
 
     # solr field configuration for document/show views
     config.show.html_title = 'title_s'
@@ -52,13 +51,13 @@ class CatalogController < ApplicationController
     # :show may be set to false if you don't want the facet to be drawn in the 
     # facet bar
     config.add_facet_field 'resourcetype_s', :label => 'Resource Type'
-    config.add_facet_field 'title_s', :label => 'Title' 
-    config.add_facet_field 'publicationdate_s', :label => 'Publication Date' 
-    config.add_facet_field 'language_s', :label => 'Language', :limit => true 
-    config.add_facet_field 'audience_s', :label => 'Audience', :limit => 20 
+    # config.add_facet_field 'title_s', :label => 'Title' 
+    # config.add_facet_field 'publicationdate_s', :label => 'Publication Date' 
+    # config.add_facet_field 'language_s', :label => 'Language', :limit => true 
+    # config.add_facet_field 'audience_s', :label => 'Audience', :limit => 20 
     config.add_facet_field 'educationlevel_s', :label => 'Education Level' 
     config.add_facet_field 'accessmode_s', :label => 'Access Mode' 
-    config.add_facet_field 'isadaptationof_s', :label => 'Is Adaptation Of', :limit => true
+    # config.add_facet_field 'isadaptationof_s', :label => 'Is Adaptation Of', :limit => true
     config.add_facet_field 'adaptationtype_s', :label => 'Adaptation Type', :limit => true
 
     # Have BL send all facet field names to Solr, which has been the default
@@ -78,7 +77,7 @@ class CatalogController < ApplicationController
     config.add_index_field 'audience_s', :label => 'Audience'
     config.add_index_field 'educationlevel_s', :label => 'Education Level' 
     config.add_index_field 'accessmode_s', :label => 'Access Mode'
-    config.add_index_field 'isadaptationof_s', :label => 'Is Adaptation Of'
+    #config.add_index_field 'isadaptationof_s', :label => 'Is Adaptation Of'
     config.add_index_field 'adaptationtype_s', :label => 'Adaptation Type'
 
     # solr fields to be displayed in the show (single result) view
@@ -112,7 +111,7 @@ class CatalogController < ApplicationController
     # since we aren't specifying it otherwise. 
     
     config.add_search_field 'all_fields', :label => 'All Fields'
-    
+    #config.add_search_field 'description_s', :label => 'Description'
 
     # Now we see how to over-ride Solr request handler defaults, in this
     # case for a BL "search field", which is really a dismax aggregate
@@ -120,36 +119,44 @@ class CatalogController < ApplicationController
     
     config.add_search_field('title_s', :label => 'Title') do |field|
       # solr_parameters hash are sent to Solr as ordinary url query params. 
-      field.solr_parameters = { :'spellcheck.dictionary' => 'title_s' }
+      field.solr_parameters = { :'spellcheck.dictionary' => 'default' }
 
       # :solr_local_parameters will be sent using Solr LocalParams
       # syntax, as eg {! qf=$title_qf }. This is neccesary to use
       # Solr parameter de-referencing like $title_qf.
       # See: http://wiki.apache.org/solr/LocalParams
-      # field.solr_local_parameters = { 
-      #   :qf => '$title_s_qf',
-      #   :pf => '$title_s_pf'
-      # }
+      field.solr_local_parameters = { 
+        :qf => '$title_s_qf',
+        :pf => '$title_s_pf'
+      }
     end
     
-    config.add_search_field('resourcetype_s', :label => 'Resource Type') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'resourcetype_s'  }
+    config.add_search_field('resourcetype_s', :label => 'Resource Type') # do |field|
+      # field.solr_parameters = { :'spellcheck.dictionary' => 'resourcetype_s'  }
       # field.solr_local_parameters = { 
       #   :qf => '$resourcetype_s_qf',
       #   :pf => '$resourcetype_s_pf'
       # }
-    end
+    # end
     
     # Specifying a :qt only to show it's possible, and so our internal automated
     # tests can test it. In this case it's the same as 
     # config[:default_solr_parameters][:qt], so isn't actually neccesary. 
-    config.add_search_field('subject_s', :label => 'Subject') do |field|
-      field.solr_parameters = { :'spellcheck.dictionary' => 'subject_s' }
+    config.add_search_field('description_t', :label => 'Description') do |field|
+      field.solr_parameters = { :'spellcheck.dictionary' => 'default' }
       field.qt = 'search'
-      # field.solr_local_parameters = { 
-      #   :qf => '$subject_s_qf',
-      #   :pf => '$subject_s_pf'
-      # }
+      field.solr_local_parameters = { 
+        :qf => '$description_t',
+        :pf => '$description_t'
+       }
+    end
+    
+    config.add_search_field('educationlevel', :label => 'Education Level') do |field|
+      field.solr_parameters = { :'spellcheck.dictionary' => 'default'  }
+       field.solr_local_parameters = { 
+         :qf => '$educationlevel_t',
+         :pf => '$educationlevel_t'
+       }
     end
 
     # "sort results by" select (pulldown)
