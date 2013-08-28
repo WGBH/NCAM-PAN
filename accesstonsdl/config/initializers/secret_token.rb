1dd8da75be18f9ca3bdeee6289af64c2d51facaa
Mitch Smith <mitch_smith@wgbh.org>
2013-08-28 13:12:06 -0400

    accesstonsdl version 2 initial commit

---
+++ accesstonsdl/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Accesstonsdl::Application.config.secret_key_base = '4cdaba29a84fb30a23b8c231306e9f09468c0f579d2c902122989e5425b978708056a5c42300b8128d5f9b8a74ea447fd204b05501a1b5ef7b1a1ffb709310b3'
