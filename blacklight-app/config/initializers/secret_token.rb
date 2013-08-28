90f8c6f76204316c5f50ef3cfeebe4a4604ed750
Mitch Smith <mitch_smith@wgbh.org>
2013-08-28 13:13:47 -0400

    removed version 1 from develop ;

--- blacklight-app/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-BlacklightApp::Application.config.secret_token = 'afb534ee43fcba576c4e3f0d29e382cb7f560852c250c20c3eaffdc22c73ea438790cd00a534f8d2798bb82572975e329dad0f6be101a3237f38781488ea4045'
--- blacklight-app/lib/tasks/ingest.rake
+++
@@ -1,8 +0,0 @@
-namespace :ingest do
-  desc "Load NCAM_AFA data"
-  task :ncamafa => :environment do
-    file = ENV['file']
-    Ingest::Ncamafa.new(file).process!
-  end
-  
-end
