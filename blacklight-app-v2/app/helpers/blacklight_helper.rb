e0dcad4f55b5d3bd6a5f70e8b4029fe064ba0000
Mitch Smith <mitch_smith@wgbh.org>
2014-07-13 23:30:39 -0400

    formats result fields

--- blacklight-app-v2/app/helpers/blacklight_helper.rb
+++ blacklight-app-v2/app/helpers/blacklight_helper.rb
@@ -7,4 +7,14 @@ module BlacklightHelper
     label = render_document_index_label doc, opts
     link_to label, doc[:url_s], target: '_blank'
   end
+
+  ##
+  # Determine whether to render a given field in the index view.
+  #  
+  # @param [SolrDocument] document
+  # @param [Blacklight::Solr::Configuration::SolrField] solr_field
+  # @return [Boolean]
+  def should_render_index_field? document, solr_field
+    should_render_field?(solr_field, document) && document_has_value?(document, solr_field) && solr_field.field != "adaptationtype_s"
+  end
 end
