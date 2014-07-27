bb770379e02d138d3dbcc9acdb1b46ac1cd0b6ab
Mitch Smith <mitch_smith@wgbh.org>
2014-07-26 21:30:33 -0400

    about page

--- blacklight-app-v2/config/routes.rb
+++ blacklight-app-v2/config/routes.rb
@@ -1,6 +1,8 @@
 BlacklightAppV2::Application.routes.draw do
   root :to => "home#index"
   blacklight_for :catalog
+
+  get 'about' => 'home#about'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
